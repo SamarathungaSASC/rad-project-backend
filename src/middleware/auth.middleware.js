9d4059aae873d6c0c9c37f0f02f26498628d7e80
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-09-08 10:01:41 +0530

    edit middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,24 +6,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const LocalStrategy = require("passport-local").Strategy;
 
-const jwtStrategy = new JwtStrategy(
-  {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: JWT_SECRET,
-  },
-  async function (jwt_payload, done) {
-    try {
-      const user = await User.findById(jwt_payload.id).select("-passwordHash");
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    } catch (error) {
-      return done(error, false);
-    }
-  }
-);
+
 
 const localStrategy = new LocalStrategy(
   {
