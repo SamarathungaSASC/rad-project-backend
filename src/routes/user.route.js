0247d822a12d36a2d8af605dde250ba9c529694a
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-09-08 10:11:41 +0530

    edit user.route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,5 +5,8 @@ const userController = require("../controllers/user.controller");
 router.get("/data", userController.getData);
 router.put("update-data", userController.updateData);
 
+router.post("/request-blood", userController.requestBlood);
+router.get("/get-requests", userController.getRequests);
+
 
 module.exports = router;
